6714f7491c809d0429e40e502a48a53094a116f2
Sidorela06 <119520024+Sidorela06@users.noreply.github.com>
2022-11-30 08:02:24 -0800

    Add initial CV files

---
+++ Portfolio/script.js
@@ -0,0 +1,10 @@
+ let submitButton = document.querySelector('#submit-button');
+ console.log(submitButton);
+
+ function clickListener() { 
+    console.log('Button clicked'); 
+  } 
+  
+  submit.addEventListener('click', clickListener);
+
+ submitButton.addEventListener('click', clickListener);
